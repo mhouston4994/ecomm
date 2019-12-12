b3d019f71797394168eb29d55ecca03ff80ac148
mhouston4994 <mhouston4994@gmail.com>
2019-12-12 10:57:04 -0700

    added seeds json, added forEach to loop items on index, added route for :id

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
+const items = require('./public/js/seed.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+//ROUTES
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render("index", {items: items});
 })
 
+app.get('/products/:id', (req,res) => {
+    res.send("Item number: " + req.params.id);
+})
+
+//SERVER LISTENING
 app.listen(3000, () => {
-    console.log("server running");
+    console.log("Server Started");
 })
---
+++ public/js/seed.js
@@ -0,0 +1,34 @@
+const inventory = [
+                 {  
+                    "id":           1,
+                    "name":         "Boots",   
+                    "Price":     "50"
+                 },
+                 {  
+                    "id":           2,
+                    "name":         "Shoes",   
+                    "price":     "40"
+                 },
+                 {  
+                    "id":           3,
+                    "name":         "Toys",   
+                    "price":     "30"
+                 },
+                 {  
+                    "id":           4,
+                    "name":         "Clothes",   
+                    "Price":     "50"
+                 },
+                 {  
+                    "id":           5,
+                    "name":         "Snacks",   
+                    "price":     "40"
+                 },
+                 {  
+                    "id":           6,
+                    "name":         "Drinks",   
+                    "price":     "30"
+                 }
+    ]
+
+module.exports = inventory;
